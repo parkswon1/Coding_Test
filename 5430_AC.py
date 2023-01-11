48190cc1af32abc27ca258cef20c41a2328de08e
parkseokwon <parkswon1@naver.com>
2023-01-11 21:43:20 +0900

    5430_AC

--- 5430_AC.py
+++ 5430_AC.py
@@ -2,46 +2,37 @@ T = int(input())
 for i in range(T):
     command = input()
     n = int(input())
-    array = input()
+    array= input().rstrip()[1:-1].split(',')
     direction = 1
-    frontindex = 1
-    backindex =len(array)-2
+    frontindex = 0
+    backindex = len(array)
     errorcode = 0
-    for j in command:
-        if j == 'R':
+    for c in command:
+        if c == 'R':
             if direction == 1:
                 direction = -1
             else:
-                direction = 1
-        elif j == 'D':
+                direction =1
+        if c == 'D':
+            if n == 0:
+                errorcode = 1
             if direction == 1:
-                if frontindex < backindex:
-                    while(1):
-                        frontindex += 1
-                        if array[frontindex] == ',' or frontindex == backindex:
-                            frontindex += 1
-                            break
+                if frontindex <backindex:    
+                    frontindex +=1
                 else:
-                    print('error')
                     errorcode = 1
-                    break
             else:
                 if backindex > frontindex:
-                    while(1):
-                        backindex -= 1
-                        if array[backindex] == ',' or frontindex == backindex:
-                            backindex -= 1
-                            break
+                    backindex -= 1
                 else:
-                    print('error')
                     errorcode = 1
-                    break
-        else:
-            print('error')
-            errorcode = 1
-            break
-    if errorcode == 0:
-        if direction == 1:
-            print('[' + "".join(array[frontindex:backindex+1]) + ']')
-        else:
-            print('[' + "".join(array[backindex:frontindex-1:-1]) + ']')
+    if errorcode == 1:
+        print('error')
+    elif n == 0:
+        print('[]')
+    elif direction == 1:
+        print('['+",".join(array[frontindex:backindex])+']')
+    elif n == 1:
+        print('['+"".join(array[0])+']')
+    else:
+        print('['+",".join(array[backindex-1:frontindex:-1])+','+array[frontindex]+']')
