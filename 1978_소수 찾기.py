52ef684ca02fa504f0b25992e786ec1fab2f3ce2
parkseokwon <parkswon1@naver.com>
2023-01-12 21:05:04 +0900

    1978_소수 찾기.py

---
+++ 1978_소수 찾기.py
@@ -0,0 +1,17 @@
+import sys
+N = int(sys.stdin.readline())
+numbers = list(map(int,sys.stdin.readline().split()))
+count = 0
+for number in numbers:
+    check = 0
+    if number == 2:
+        count +=1
+    if number != 1:
+        if number%2 != 0:
+            for j in range(3,number-1,2):
+                if number%j == 0:
+                    check = 1
+                    break
+            if check == 0:
+                count +=1
+print(count)
