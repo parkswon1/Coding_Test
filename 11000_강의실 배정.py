3668afbfc1b435c866d8d258ea18cb5ba4257bc3
parkseokwon <parkswon1@naver.com>
2023-03-03 21:05:07 +0900

    11000_강의실 배정

--- 11000_강의실 배정.py
+++ 11000_강의실 배정.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 N = int(sys.stdin.readline())
 
@@ -10,13 +11,8 @@ time.sort()
 room = [time[0][1]]
 
 for i in range(1,N):
-    check = 0
-    for j in range(len(room)):
-        if time[i][0] >= room[j]:
-            room[j] = time[i][1]
-            check = 1
-            break
-    if check == 0:
-        room.append(time[i][1])
-
+    if time[i][0] >= room[0]:
+        heapq.heappop(room)
+    heapq.heappush(room, time[i][1])
+        
 print(len(room))
