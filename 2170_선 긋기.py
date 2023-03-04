eb5d2b3678e349491f7aefce8ef0a48384771f78
parkseokwon <parkswon1@naver.com>
2023-03-05 00:04:33 +0900

    어떤 조건이 없는지 오류

---
+++ 2170_선 긋기.py
@@ -0,0 +1,24 @@
+import sys
+N = int(sys.stdin.readline())
+
+lines = []
+
+for i in range(N):
+    lines.append(list(map(int,sys.stdin.readline().split())))
+
+lines.sort()
+
+front = lines[0][0]
+back = lines[0][1]
+
+output = 0
+
+for i in range(1,N):
+    if lines[i][0] <= back:
+        back = lines[i][1]
+    else:
+        output += abs(back - front)
+        front = lines[i][0]
+        back = lines[i][1]
+
+print(output + abs(back - front))
