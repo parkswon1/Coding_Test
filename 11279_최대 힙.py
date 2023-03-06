91efe2ec4648c9d0ba5421929a2fc5fea856cea9
parkseokwon <parkswon1@naver.com>
2023-03-06 23:15:54 +0900

    11279_최대 힙.py

---
+++ 11279_최대 힙.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+N = int(sys.stdin.readline())
+
+heap = []
+for i in range(N):
+    infromation = int(sys.stdin.readline())
+    if infromation == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap,(-infromation,infromation))
