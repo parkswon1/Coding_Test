eadb58fd908ae34186d201d0d364ce7b4ee1b267
parkseokwon <parkswon1@naver.com>
2023-03-22 16:46:38 +0900

    7785_회사에 있는 사람

--- 7785_회사에 있는 사람.py
+++ 7785_회사에 있는 사람.py
@@ -1,24 +1,14 @@
-import heapq
-
 import sys
 n = int(sys.stdin.readline())
 
-Heap = []
-
-for nm in range(n):
+dic = {}
+for i in range(n):
     Input = list(sys.stdin.readline().split())
-    heapq.heappush(Heap, Input[0])
+    if Input[1] == 'enter':
+        dic[Input[0]] = 'enter'
+    else:
+        del dic[Input[0]]
 
-A = heapq.heappop(Heap)
-Output = []
-for n in range(n-1):
-    B = heapq.heappop(Heap)
-    if A != B:
-        Output.append(A)
-        A = B
-
-if B != Output[-1]:
-    Output.append(B)
-
-for o in range(-1,-len(Output)-1,-1):
-    print(Output[o])
+Output = sorted(dic.keys(),reverse=True)
+for o in Output:
+    print(o)
