b9a8b3f8b0076b772dd0699fd0dd6b5259368364
parkseokwon <parkswon1@naver.com>
2023-10-30 21:48:49 +0900

    1209.Sum.py

---
+++ SW Expert Academy/1209.Sum.py
@@ -0,0 +1,25 @@
+test_case = int(input())
+
+boxs = []
+last_box = []
+
+for i in range(100):
+    boxs.append(list(map(int,input().split())) + [0])
+        
+    
+boxs.append([0]*101)
+right = 0
+left = 0
+for y in range(100):
+    for x in range(100):
+        boxs[y][100] += boxs[y][x]
+        boxs[100][x] += boxs[y][x]
+        if  y == x:
+            right += boxs[y][x]
+        if  y + x == 100:
+            left += boxs[y][x]
+            
+    last_box.append(boxs[y][100])
+    
+last_box += boxs[100] + [right] + [left]
+print(f'#{test_case}',max(last_box))
