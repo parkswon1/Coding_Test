29decd472ff2c14f56afc86e33c136e09d452081
parkswon1 <parkswon1>
2023-11-13 22:13:00 +0900

    1226.미로1.py

--- SW Expert Academy/1209.Sum.py
+++ SW Expert Academy/1209.Sum.py
@@ -1,25 +1,25 @@
-test_case = int(input())
+for test_case in range(1,11):
+    test_case = int(input())
+    boxs = []
+    last_box = []
 
-boxs = []
-last_box = []
-
-for i in range(100):
-    boxs.append(list(map(int,input().split())) + [0])
-        
-    
-boxs.append([0]*101)
-right = 0
-left = 0
-for y in range(100):
-    for x in range(100):
-        boxs[y][100] += boxs[y][x]
-        boxs[100][x] += boxs[y][x]
-        if  y == x:
-            right += boxs[y][x]
-        if  y + x == 100:
-            left += boxs[y][x]
+    for i in range(100):
+        boxs.append(list(map(int,input().split())) + [0])
             
-    last_box.append(boxs[y][100])
-    
-last_box += boxs[100] + [right] + [left]
-print(f'#{test_case}',max(last_box))
+        
+    boxs.append([0]*101)
+    right = 0
+    left = 0
+    for y in range(100):
+        for x in range(100):
+            boxs[y][100] += boxs[y][x]
+            boxs[100][x] += boxs[y][x]
+            if  y == x:
+                right += boxs[y][x]
+            if  y + x == 100:
+                left += boxs[y][x]
+                
+        last_box.append(boxs[y][100])
+        
+    last_box += boxs[100] + [right] + [left]
+    print(f'#{test_case}',max(last_box))
