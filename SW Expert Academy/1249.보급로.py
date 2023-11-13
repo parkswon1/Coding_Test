0c4d8d42c5941db201e5f340237d5c8eef66fce7
parkswon1 <parkswon1>
2023-11-13 20:43:10 +0900

    bfs로 완료

--- SW Expert Academy/1249.보급로.py
+++ SW Expert Academy/1249.보급로.py
@@ -1,29 +1,34 @@
-ds = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+def bfs(before):
+    after = []
 
-def isField(i, j):
-    return 0 <= i < N and 0 <= j < N
+    for node in before:
+        y = node[0]
+        x = node[1]
+        for yx in mv:
+            mx = x + yx[1]
+            my = y + yx[0]
+            if N > mx > -1 and N > my > -1:
+                if score[my][mx] > score[y][x] + board[my][mx]:
+                    score[my][mx] = score[y][x] + board[my][mx]
+                    after.append([my,mx])
 
-def bfs(i, j):
-    queue = [(i, j)]
-    while queue:
-        temp = queue[:]
-        queue = []
-        while temp:
-            si, sj = temp.pop()
-            for di, dj in ds:
-                ni = si + di
-                nj = sj + dj
-                if isField(ni, nj) and (not visited[ni][nj] or shortcut[ni][nj] > shortcut[si][sj] + g[ni][nj]):
-                    shortcut[ni][nj] = shortcut[si][sj] + g[ni][nj]
-                    queue.append((ni, nj))
-                    visited[ni][nj] = 1
+    if len(after) == 0:
+        return
+    else:
+        bfs(after)
 
-for i in range(int(input())):
+T = int(input())
+
+mv = [[1,0],[-1,0],[0,1],[0,-1]]
+for i in range(1,T+1):
     N = int(input())
-    shortcut = [[90001] * N for _ in range(N)]
-    shortcut[0][0] = 0
-    visited = [[0] * N for _ in range(N)]
-    g = [[int(i) for i in list(input())] for _ in range(N)]
-    time_sum = 0
-    bfs(0, 0)
-    print('#{} {}'.format(i+1, shortcut[N-1][N-1]))
+    board = []
+    score = []
+    for n in range(N):
+        score.append([999999]*N)
+        board.append([int(i) for i in input()])
+
+    score[0][0] = 0
+    bfs([[0,0]])
+
+    print(f'#{i}', score[N-1][N-1])
