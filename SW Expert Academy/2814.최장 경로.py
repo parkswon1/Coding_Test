459016322cdd404b4d2c0e3eeb40115e4521f1a8
parkswon1 <parkswon1@naver.com>
2023-11-18 22:28:47 +0900

    2814.최장 경로.py

--- SW Expert Academy/2814.최장 경로.py
+++ SW Expert Academy/2814.최장 경로.py
@@ -1,10 +1,10 @@
-def DFS(n,count,visit):
+def DFS(n,count):
     global score
     score = max(count,score)
     for node in nodes[n]:
         if visit[node] != 1:
             visit[node] = 1
-            DFS(node,count+1,visit)
+            DFS(node,count+1)
             visit[node] = 0
 
 T = int(input())
@@ -20,7 +20,5 @@ for test_case in range(1,T+1):
             nodes[x].append(y)
             nodes[y].append(x)
         for n in range(1,N+1):
-            visit[n] = 1
-            DFS(n,0,[0]*(N+1))
-            visit[n] = 0
+            DFS(n,0)
     print(f'#{test_case}',score)
