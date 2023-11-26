88e753bb378e06209fcd5208deb792f96c5b1aab
parkseokwon <parkswon1@naver.com>
2023-11-26 17:12:30 +0900

    14888_연산자 끼워넣기.py

---
+++ 백준/14888_연산자 끼워넣기.py
@@ -0,0 +1,41 @@
+def search(n,cals,count):
+    global high, low
+    if count == N:
+        if n < low:
+            low = n
+        if n > high:
+            high = n
+        return
+
+    if cals[0] != 0:
+        cals[0] -= 1
+        search(n + A[count], cals, count + 1)
+        cals[0] += 1
+    if cals[1] != 0:
+        cals[1] -= 1
+        search(n - A[count], cals, count + 1)
+        cals[1] += 1
+    if cals[2] != 0:
+        cals[2] -= 1
+        search(n * A[count], cals, count + 1)
+        cals[2] += 1
+    if cals[3] != 0:
+        cals[3] -= 1
+        if n < 0:
+            number = (-n//A[count])*(-1)
+        else:
+            number = n//A[count]
+        search(number, cals, count + 1)
+        cals[3] += 1
+
+N = int(input())
+
+A = list(map(int,input().split()))
+cals = list(map(int,input().split()))
+high = -float('inf')
+low = float('inf')
+
+search(A[0],cals,1)
+
+print(high)
+print(low)
