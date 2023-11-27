e89af0381331ce6087c0391beb0647b88eff2656
parkseokwon <parkswon1@naver.com>
2023-11-27 23:29:27 +0900

    20055_컨베이어 벨트 위의 로봇.py

---
+++ 백준/20055_컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+N, K = map(int,sys.stdin.readline().split())
+A = deque(list(map(int,sys.stdin.readline().split())))
+count = 0
+stage = 0
+robots = [0]*N
+while(1):
+    A.appendleft(A.pop())
+    for n in range(N - 2, -1, -1):
+        if robots[n] == 1:
+            robots[n] = 0
+            robots[n + 1] = 1
+    if robots[N - 1] == 1:
+        robots[N - 1] = 0
+    for n in range(N - 2, -1, -1):
+        if robots[n] == 1 and robots[n + 1] == 0 and A[n + 1] != 0:
+            robots[n] = 0
+            robots[n + 1] = 1
+            A[n + 1] -= 1
+            if A[n + 1] == 0:
+                count += 1
+    if robots[N - 1] == 1:
+        robots[N - 1] = 0
+
+    if A[0] != 0:
+        A[0] -= 1
+        robots[0] = 1
+        if A[0] == 0:
+            count += 1
+    stage += 1
+    if count >= K:
+        break
+
+print(stage)
