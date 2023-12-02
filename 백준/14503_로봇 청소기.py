93a61da75a51b24005e42b652d761450536d7375
parkseokwon <parkswon1@naver.com>
2023-12-02 18:22:36 +0900

    14503_로봇 청소기.py

--- 백준/14503_로봇 청소기.py
+++ 백준/14503_로봇 청소기.py
@@ -1,38 +1,33 @@
-def search(x,y):
-    global d, count
+def search(y,x):
+    global d,count
     if room[y][x] == 0:
-        count += 1
         room[y][x] = 2
+        count += 1
     check = 0
-    for mv in move:
-        mx = x + mv[1]
-        my = y + mv[0]
+    for i in range(1,5):
+        d = (d + 1)%4
+        my = move[d][0] + y
+        mx = move[d][1] + x
         if room[my][mx] == 0:
-            check == 1
+            check = 1
+            break
     if check == 0:
-        mx = x - move[d][1]
         my = y - move[d][0]
-        if room[my][mx] != 1:
-            search(mx,my)
-    else:
-        for i in range(1, 5):
-            d = (d + i)%4
-            mx = x + move[d][1]
-            my = y + move[d][0]
-            if room[my][mx] == 0:
-                search(mx,my)
+        mx = x - move[d][1]
+        if room[my][mx] == 1:
+            return
+    search(my,mx)
 
 import sys
 sys.setrecursionlimit(10**7)
-
-N,M = map(int,sys.stdin.readline().split())
-move = [[1,0],[0,1],[-1,0],[0,-1]]
+move = [[-1,0],[0,-1],[1,0],[0,1]]
+N, M = map(int,sys.stdin.readline().split())
 r,c,d = map(int,sys.stdin.readline().split())
-
+if d == 1 or d == 3:
+    d = (d+2)%4
 room = []
-count = 0
 for n in range(N):
-    room.append(list(map(int,input().split())))
-search(c,r)
-
+    room.append(list(map(int,sys.stdin.readline().split())))
+count = 0
+search(r,c)
 print(count)
