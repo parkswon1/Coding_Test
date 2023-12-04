4b29a18b15ecc2fd2635b94c95ce5a16d5441816
parkseokwon <parkswon1@naver.com>
2023-12-04 23:26:52 +0900

    3,4번예제가 안되는중

---
+++ 백준/14891_톱니바퀴.py
@@ -0,0 +1,49 @@
+def spin(N,D):
+    if D == 1:
+        gears[N].appendleft(gears[N].pop())
+    else:
+        gears[N].append(gears[N].popleft())
+
+import sys
+from collections import deque
+
+gears = []
+for _ in range(4):
+    gears.append(deque([int(i) for i in sys.stdin.readline().rstrip()]))
+
+K = int(input())
+
+for k in range(K):
+    N,D = map(int,sys.stdin.readline().split())
+    N -= 1
+    left = gears[N][6]
+    right = gears[N][2]
+    spin(N,D)
+    d = -D
+    for n in range(N+1,4):
+        rightleft = gears[n][6]
+        if rightleft == right:
+            spin(n,d)
+            d = -d
+            right = gears[n][6]
+        else:
+            break
+    d = -D
+    for n in range(N-1,-1,-1):
+        leftright = gears[n][2]
+        if leftright == left:
+            spin(n,d)
+            d = -d
+            left = gears[n][2]
+        else:
+            break
+    print(gears)
+
+
+output = 0
+score = [1,2,4,8]
+for i in range(4):
+    if gears[i][0] == 1:
+        output += score[i]
+
+print(output)
