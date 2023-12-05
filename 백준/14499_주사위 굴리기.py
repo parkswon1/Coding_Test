dcca657cafb3f96875f7f4d9f189880d21e1efad
parkseokwon <parkswon1@naver.com>
2023-12-05 21:52:40 +0900

    예제 4번에서 막히는중

---
+++ 백준/14499_주사위 굴리기.py
@@ -0,0 +1,52 @@
+def xspin(d):
+    if d == 1:
+        bottom = xdice.pop()
+        xdice.appendleft(ydice.pop())
+    else:
+        bottom = xdice.popleft()
+        xdice.append(ydice.pop())
+
+    ydice.append(bottom)
+    ydice[1] = xdice[1]
+
+def yspin(d):
+    if d == 1:
+        ydice.appendleft(ydice.pop())
+    else:
+        ydice.append(ydice.popleft())
+
+    xdice[1] = ydice[1]
+
+import sys
+from collections import deque
+
+N,M,x,y,K = map(int,sys.stdin.readline().split())
+ydice = deque([0,0,0,0])
+xdice = deque([0,0,0])
+board = []
+for n in range(N):
+    board.append(list(map(int,sys.stdin.readline().split())))
+
+command = list(map(int,sys.stdin.readline().split()))
+move = [[0,1],[0,-1],[-1,0],[1,0]]
+for c in command:
+    c -= 1
+    mx = x + move[c][1]
+    my = y + move[c][0]
+    if N > my > -1 and M > mx > -1:
+        x = mx
+        y = my
+        if c == 0:
+            xspin(1)
+        elif c == 1:
+            xspin(-1)
+        elif c == 2:
+            yspin(-1)
+        else:
+            yspin(1)
+        if board[y][x] != 0:
+            ydice[-1] = board[y][x]
+        else:
+            board[y][x] = ydice[-1]
+            ydice[-1] = 0
+        print(ydice[1])
