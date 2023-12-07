895705f18e8a05967135a4ede6e60c0446876798
parkseokwon <parkswon1@naver.com>
2023-12-07 21:50:49 +0800

    방문 기록을 아직 처리못하는중

--- 백준/2573_빙산.py
+++ 백준/2573_빙산.py
@@ -1,29 +1,74 @@
-def DFS(Node):
-    global Year
-    if len(Node) == 0:
+def bfs(ice):
+    global year,count
+    year += 1
+    new_ice = []
+    ice_count = len(ice)
+    minus = [0] * ice_count
+    for _ in range(ice_count):
+        i = ice[_]
+        visit[i[0]][i[1]] = 0
+        for s in surround:
+            mx = i[1] + s[1]
+            my = i[0] + s[0]
+            if N > my > -1 and M > mx > -1:
+                if board[my][mx] == 0:
+                    minus[_] += 1
+
+    for k in range(ice_count):
+        x = ice[k][1]
+        y = ice[k][0]
+        board[y][x] -= minus[k]
+        if board[y][x] > 0:
+            new_ice.append([y,x])
+        else:
+            board[y][x] = 0
+    visit[new_ice[0][0]][new_ice[0][1]] = 1
+    count = 1
+    search([[new_ice[1][0],new_ice[1][1]]])
+
+    if count < len(new_ice):
+        print(year)
+        return
+    if len(new_ice) == 0:
         print(0)
         return
-    for n in range(N):
-       
+    else:
+        bfs(new_ice)
+
+def search(bings):
+    global count
+    new_bings = []
+    for b in bings:
+        for s in surround:
+            my = b[0] + s[0]
+            mx = b[1] + s[1]
+            if N > my > -1 and M > mx > -1:
+                if visit[my][mx] == 0:
+                    count += 1
+                    visit[my][mx] = 1
+                    new_bings.append([my,mx])
+    if len(new_bings) == 0:
+        return
+    else:
+        search(new_bings)
 
 
+import sys
+sys.setrecursionlimit(10**7)
 
-
-
-
-
-N, M = list(map(int,input().split()))
-
-Map = []
-Count_Map = []
-Ice_Mountains = []
-Year = 0
+surround = [[1,0],[-1,0],[0,1],[0,-1]]
+N, M = map(int,sys.stdin.readline().split())
+board = []
+ice = []
+visit = []
+year = 0
+count = 1
 for n in range(N):
-    Count_Map.append([0]*M)
-    Line = list(map(int,input().split()))
+    visit.append([0]*M)
+    line = list(map(int,sys.stdin.readline().split()))
     for m in range(M):
-        if Line[m] != 0:
-            Ice_Mountains.append([n,m])
-            Count_Map[n][m] = 1
-            
-    Map.append(Line)
+        if line[m] != 0:
+            ice.append([n,m])
+    board.append(line)
+
+bfs(ice)
