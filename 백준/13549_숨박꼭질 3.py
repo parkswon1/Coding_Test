00bdfa615b0390848b9a6c118f5b0419343599d7
parkseokwon <parkswon1@naver.com>
2023-12-08 02:18:18 +0800

    시간값 계산 오류

---
+++ 백준/13549_숨박꼭질 3.py
@@ -0,0 +1,32 @@
+def dfs(nodes):
+    global low
+    new_nodes = []
+    for n in nodes:
+        if n[0] == K:
+            low = min(low,n[1])
+        next = n[0] * 2
+        if 100001 > next > -1 and index[next] > n[1]:
+            new_nodes.append([next,n[1]])
+            index[next] = 1
+        next = n[0] + 1
+        if 100001 > next > -1 and index[next] > n[1] + 1:
+            new_nodes.append([next, n[1]+1])
+            index[next] = 1
+        next = n[0] - 1
+        if 100001 > next > -1 and index[next] > n[1] + 1:
+            new_nodes.append([next, n[1]+1])
+            index[next] = 1
+    if len(new_nodes) == 0:
+        return
+    dfs(new_nodes)
+
+import sys
+sys.setrecursionlimit(10**7)
+
+index = [0]*(100001)
+low = float('inf')
+N, K = map(int,sys.stdin.readline().split())
+index[N] = float('inf')
+dfs([[N,0]])
+
+print(low)
