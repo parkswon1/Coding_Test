2a2bbb256bedb53820970e05557f2d7daf627dbb
parkseokwon <parkswon1@naver.com>
2023-12-10 01:41:40 +0900

    2573_빙산.py

--- 백준/2573_빙산.py
+++ 백준/2573_빙산.py
@@ -1,74 +1,68 @@
-def bfs(ice):
-    global year,count
-    year += 1
-    new_ice = []
-    ice_count = len(ice)
-    minus = [0] * ice_count
-    for _ in range(ice_count):
-        i = ice[_]
-        visit[i[0]][i[1]] = 0
-        for s in surround:
-            mx = i[1] + s[1]
-            my = i[0] + s[0]
+def melt(node):
+    global count, output
+    output += 1
+    new_node = []
+    for n in node:
+        visit[n[0]][n[1]] = 0
+        for ne in near:
+            my = n[0] + ne[0]
+            mx = n[1] + ne[1]
             if N > my > -1 and M > mx > -1:
                 if board[my][mx] == 0:
-                    minus[_] += 1
-
-    for k in range(ice_count):
-        x = ice[k][1]
-        y = ice[k][0]
-        board[y][x] -= minus[k]
-        if board[y][x] > 0:
-            new_ice.append([y,x])
+                    n[2] -= 1
+    for n in node:
+        if n[2] <= 0:
+            board[n[0]][n[1]] = 0
         else:
-            board[y][x] = 0
-    visit[new_ice[0][0]][new_ice[0][1]] = 1
-    count = 1
-    search([[new_ice[1][0],new_ice[1][1]]])
+            board[n[0]][n[1]] = n[2]
+            new_node.append(n)
 
-    if count < len(new_ice):
-        print(year)
-        return
-    if len(new_ice) == 0:
+    if len(new_node) == 0:
         print(0)
         return
-    else:
-        bfs(new_ice)
 
-def search(bings):
-    global count
-    new_bings = []
-    for b in bings:
-        for s in surround:
-            my = b[0] + s[0]
-            mx = b[1] + s[1]
-            if N > my > -1 and M > mx > -1:
-                if visit[my][mx] == 0:
-                    count += 1
-                    visit[my][mx] = 1
-                    new_bings.append([my,mx])
-    if len(new_bings) == 0:
+    count = 1
+    visit[new_node[0][0]][new_node[0][1]] = 1
+    count_bing([new_node[0]])
+
+    if count != len(new_node):
+        print(output)
         return
     else:
-        search(new_bings)
+        melt(new_node)
 
+def count_bing(node):
+    global count
+    new_node = []
+    for n in node:
+        for ne in near:
+            my = n[0] + ne[0]
+            mx = n[1] + ne[1]
+            if N > my > -1 and M > mx > -1:
+                if board[my][mx] != 0 and visit[my][mx] == 0:
+                    visit[my][mx] = 1
+                    count += 1
+                    new_node.append([my,mx])
+    if len(new_node) == 0:
+        return
+    else:
+        count_bing(new_node)
 
 import sys
 sys.setrecursionlimit(10**7)
-
-surround = [[1,0],[-1,0],[0,1],[0,-1]]
 N, M = map(int,sys.stdin.readline().split())
+
 board = []
-ice = []
 visit = []
-year = 0
-count = 1
+node = []
+near = [[1,0],[-1,0],[0,1],[0,-1]]
+output = 0
 for n in range(N):
-    visit.append([0]*M)
     line = list(map(int,sys.stdin.readline().split()))
+    visit.append([0]*M)
     for m in range(M):
         if line[m] != 0:
-            ice.append([n,m])
+            node.append([n,m,line[m]])
     board.append(line)
 
-bfs(ice)
+melt(node)
