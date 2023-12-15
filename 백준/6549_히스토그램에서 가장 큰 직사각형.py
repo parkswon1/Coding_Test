03b8937720b3a933d9f6b1e9b3faefe0589be547
parkseokwon <parkswon1@naver.com>
2023-12-15 10:05:55 +0900

    6549_히스토그램에서 가장 큰 직사각형.py

--- 백준/6549_히스토그램에서 가장 큰 직사각형.py
+++ 백준/6549_히스토그램에서 가장 큰 직사각형.py
@@ -9,14 +9,15 @@ while(1):
     output = -1
     for n in range(1,N+1):
         now = numbers[n]
+        start = n
         while(stack):
             if stack[-1][0] > now:
                 output = max(output, stack[-1][0] * (n-stack[-1][1]))
+                start = stack[-1][1]
                 stack.pop()
             else:
                 break
-        stack.append([now,n])
-        print(stack,output)
+        stack.append([now,start])
     for i in range(len(stack)):
         output = max(output, stack[i][0] * (N+1-stack[i][1]))
 
