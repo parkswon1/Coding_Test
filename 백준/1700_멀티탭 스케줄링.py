81b16ac3109ab26dc5852a91c99073d650143da8
parkseokwon <parkswon1@naver.com>
2023-12-18 15:50:42 +0900

    1700_멀티텝 스케줄링.py

--- 백준/1700_멀티탭 스케줄링.py
+++ 백준/1700_멀티탭 스케줄링.py
@@ -4,27 +4,28 @@ N, K = map(int,sys.stdin.readline().split())
 product = list(map(int,sys.stdin.readline().split()))
 
 plug = []
-pcount = [0]*101
-for p in product:
-    pcount[p] += 1
-
 output = 0
-for p in product:
-    pcount[p] -= 1
-    if len(plug) != N and p not in plug:
-        plug.append(p)
-    else:
-        lowIndex = -1
-        lowCount = float('inf')
-        for n in range(N):
-            if plug[n] == p:
-                lowIndex = -1
-                break
-            elif pcount[plug[n]] < lowCount:
-                lowCount = pcount[plug[n]]
-                lowIndex = n
-        if lowIndex != -1:
-            plug[lowIndex] = p
+for k in range(K):
+    if product[k] not in plug:
+        if len(plug) != N:
+            plug.append(product[k])
+        else:
+            check = [float('inf')]*N
+            for i in range(N):
+                for j in range(k,K):
+                    if product[j] == plug[i]:
+                        check[i] = j
+                        break
+
+            maxIndex = 0
+            maxCount = 0
+            for n in range(N):
+                if check[n] > maxCount:
+                    maxIndex = n
+                    maxCount = check[n]
+
+            plug[maxIndex] = product[k]
+
             output += 1
 
 print(output)
