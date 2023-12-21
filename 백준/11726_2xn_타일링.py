33705658e0d015788fef3280c2575d2ccf4af8dd
parkseokwon <parkswon1@naver.com>
2023-12-21 14:10:44 +0900

    11726_2xn_타일링.py

---
+++ 백준/11726_2xn_타일링.py
@@ -0,0 +1,15 @@
+N= int(input())
+
+dp1 = 1
+if N > 1:
+    dp2 = 2
+    for n in range(3,N+1):
+        dpn = dp1 + dp2
+        dp1 = dp2
+        dp2 = dpn
+if N == 1:
+    print(dp1)
+elif N == 2:
+    print(dp2)
+else:
+    print(dpn%10007)
