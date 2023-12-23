30695f0601bebee99040baec3bdb3bb05fe004c9
parkseokwon <parkswon1@naver.com>
2023-12-23 19:03:17 +0900

    11053_가장 긴 감소하는 부분 수열.py

--- 백준/11053_가장 긴 감소하는 부분 수열.py
+++ 백준/11053_가장 긴 감소하는 부분 수열.py
@@ -1,13 +1,16 @@
 import sys
 N = int(sys.stdin.readline())
-numbers = list(map(int,sys.stdin.readline().split()))
 
-count = [1]*(N)
-Min = numbers[0]
+dp = [0] * N
+numbers = list(map(int, sys.stdin.readline().split()))
+dp[0] = 1
 
-for i in range(N):
-    for j in range(i):
-        if numbers[j] < numbers[i]:
-            count[i] = max(count[i],count[j]+1)
-            
-print(max(count))
+output = dp[0]
+for i in range(1,N):
+    temp = 0
+    for j in range(i-1,-1,-1):
+        if numbers[j] > numbers[i]:
+            temp = max(dp[j], temp)
+    dp[i] = 1 + temp
+    output = max(dp[i], output)
+print(output)
