1306f87ebb4085399d8f5a6f30063e4c75a6944e
parkseokwon <parkswon1@naver.com>
2023-12-27 17:19:56 +0900

    택배 빼주는 알고리즘이 문제임

--- 백준/8980_택배.py
+++ 백준/8980_택배.py
@@ -1,34 +1,43 @@
 import sys
-from collections import deque
-
-N, C = list(map(int,sys.stdin.readline().split()))
+import heapq
+N, C = map(int,sys.stdin.readline().split())
 M = int(sys.stdin.readline())
-
-sendingInfo = []
+sendingBox = []
 for m in range(M):
-    sendingInfo.append(list(map(int,sys.stdin.readline().split())))
+    heapq.heappush(sendingBox,list(map(int,sys.stdin.readline().split())))
 
-sendingInfo.sort()
-sendingInfo = deque(sendingInfo)
-
-cargo = deque([])
-maxC = 0
+cargo = [0]*(N + 1)
+lastindex = 0
+totalCargo = 0
 
 output = 0
-
 for n in range(1,N+1):
-    while(cargo):
-        if cargo[0][0] == n:
-            maxC -= cargo[0][1]
-        cargo.popleft()
-            
-    while(sendingInfo):
-        if sendingInfo[0][0] == n:
-            if maxC != C:
-                maxC += sendingInfo[0][2]
-                diff = maxC - C
-                if diff > 0:
-                    maxC = C
-                    sendingInfo[0][2] -= diff
-                cargo.append([sendingInfo[0][1],sendingInfo[0][2]])
-            sendingInfo.popleft()
+    output += cargo[n]
+    totalCargo -= cargo[n]
+
+    while(sendingBox):
+        if sendingBox[0][0] == n: ##보내는 마을에 도착한 상자라면
+            box = heapq.heappop(sendingBox) ##일단 상자 넣기
+            lastindex = max(box[1],lastindex)
+            cargo[box[1]] += box[2]
+            totalCargo += box[2]
+            diff = C - totalCargo
+
+            while diff < 0: ##만약 상자 넣었는데 최대 용량 초과라면 초과량 만큼 가장 늦게 주는 마을 상자부터 하차 !!
+                cargo[lastindex] += diff
+                if cargo[lastindex] < 0:
+                    diff = (-cargo[lastindex])
+                    cargo[lastindex] = 0
+
+                    while lastindex > 0: ##lastindex앞으로 움직이기
+                        lastindex -= 1
+                        if cargo[lastindex] != 0:
+                            break
+                else:
+                    diff = 0
+        else:
+            break
+    if totalCargo > C :
+        totalCargo = C
+
+print(output)
