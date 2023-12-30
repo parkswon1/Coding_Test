d7e34410d93a1f0e9ca817bca6ac3ff2b6204625
parkseokwon <parkswon1@naver.com>
2023-12-30 18:37:55 +0900

    짝수 홀수를 나눠서 시간을 줄여야 할듯

--- 백준/17071_숨바꼭질 5.py
+++ 백준/17071_숨바꼭질 5.py
@@ -3,26 +3,19 @@ from collections import deque
 
 def BFS():
     while(que):
-        n, count, k = que.popleft()
+        n, count = que.popleft()
         for mn in (n + 1, n - 1, 2 * n):
             mcount = count + 1
-            mk = k + mcount
             check = count % 2
-            if 500001 > mn > -1 and 500001 > k > -1 and visited[mn][check] != mn:
-                visited[mn][check] = 1
-                if visited[mk][check] != 0:
-                    print(mcount)
-                    return
-                que.append([mn,mcount,mk])
+            if 500001 > mn > -1 and visited[mn][check] != mn:
+                visited[mn][check] = mcount
+                que.append([mn,mcount])
 
-    print(-1)
-    return
 
-sys.setrecursionlimit(10**7)
 N, K = map(int,sys.stdin.readline().split())
 visited = [[0,0] for i in range(500001)]
 visited[N][0] = 1
-que = deque([[N,0,K]])
+que = deque([[N,0]])
 if N == K:
     print(0)
 else:
