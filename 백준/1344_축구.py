fad3d9b6157f97dacbacf66922257c2cff570072
parkswon1 <parkswon1@naver.com>
2024-01-05 01:27:00 +0900

    1344_축구.py

--- 백준/1344_축구.py
+++ 백준/1344_축구.py
@@ -1,26 +1,26 @@
-import sys
+a = float(input())
+b = float(input())
+a /= 100
+b /= 100
+p = "0011010100010100010"
 
-A = int(sys.stdin.readline())
-B = int(sys.stdin.readline())
-time = 90//5
-dp1 = []
-dp2 = []
-for n in range(19):
-    dp1.append([0]*19)
-    dp2.append([0]*19)
-dp1[0][0] = 1
-dp2[0][0] = 1
+cache = [[[None] * 20 for _ in range(20)] for _ in range(20)]
 
-for i in range(1,19):
-    for j in range(i):
-        dp1[i][j] += dp1[i-1][j] * ((100 - A)/100)
-        dp1[i][j+1] += dp1[i-1][j] * ((100 - A)/100)
 
-        dp2[i][j] += dp2[i - 1][j] * ((100 - B)/100)
-        dp2[i][j + 1] += dp2[i - 1][j] * ((100 - B)/100)
+def Sol(cur, t1, t2):
+    if cur == 18:
+        return p[t1] == '1' or p[t2] == '1'
 
-output = 0
-for i in (2,3,5,7,13,17):
-    output += dp1[18][i-1] + dp2[18][i-1]
-sum(dp1[18])
-print(output)
+    if cache[cur][t1][t2] is not None:
+        return cache[cur][t1][t2]
+
+    ret = (1 - a) * (1 - b) * Sol(cur + 1, t1, t2) + \
+          a * (1 - b) * Sol(cur + 1, t1 + 1, t2) + \
+          (1 - a) * b * Sol(cur + 1, t1, t2 + 1) + \
+          a * b * Sol(cur + 1, t1 + 1, t2 + 1)
+
+    cache[cur][t1][t2] = ret
+    return ret
+
+
+print(f"{Sol(0, 0, 0):.20f}")
