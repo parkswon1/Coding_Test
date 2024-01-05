9aa70ea377045198f5d585d9ea0c037aa2479c3a
parkseokwon <parkswon1@naver.com>
2024-01-05 21:40:47 +0900

    17144_미세먼지 안녕!.py

--- 백준/17144_미세먼지 안녕!.py
+++ 백준/17144_미세먼지 안녕!.py
@@ -1,83 +1,65 @@
 import sys
-from collections import deque
-
-def search():
-    global output
-    flag = 1
-    while dust:
-        r, c, f, t = dust.popleft()
-        if t == T:
-            print(output)
-            return output
-
-        if f != flag:
-            flag = abs(flag - 1)
-            clean(flag)
-
-        if board[f][r][c] != 0:
-            diff = board[f][r][c]//5
-            for mv in move1:
-                my = r + mv[0]
-                mx = c + mv[1]
-                if R > my > -1 and C > mx > -1:
-                    if board[abs(flag - 1)][my][mx] == 0
-                        dust.append([my,mx,abs(flag - 1),t+1])
-                    board[f][r][c] -= diff
-                    board[abs(flag - 1)][my][mx] += diff
-
-            board[abs(flag - 1)][my][mx] += board[f][r][c]
-
-
-def clean(flag):
-    now = [r - 1, 0]
-    d = 0
-    temp1 = 0
-    while ture:
-        my = now[0] + move1[d][0]
-        mx = now[1] + move1[d][1]
-        if R > my > -1 and C > mx > -1:
-            temp2 = board[flag][my][mx]
-            if temp2 == 0:
-                dust.append([my,mx,flag - 1,t+1])
-            board[flag][my][mx] = temp1
-            temp1 = temp2
-        elif board[flag][my][mx] == -1:
+R,C,T = map(int, sys.stdin.readline().split())
+pan = [list(map(int, sys.stdin.readline().split())) for _ in range(R)]
+cur = []
+cleaner = []
+def spread():
+    while cur:
+        x,y,value = cur.pop()
+        count = 0
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx<0 or ny<0 or nx>=R or ny>=C:
+                continue
+            if (nx,ny) in cleaner:
+                continue
+            pan[nx][ny]+=value//5
+            count+=1
+        pan[x][y] -= (value//5)*count
+def clean(a,b):
+    x, y = a, 1
+    index = 1
+    temp = 0
+    while True:
+        nx = x+dx[index]
+        ny = y+dy[index]
+        if nx==R or ny == C or nx==-1 or ny==-1:
+            index = (index-1)%4
+            continue
+        if x==a and y==0:
             break
-        else:
-            d += 1
-
-    now = [r, 0]
-    d = 0
-    temp1 = 0
-    while ture:
-        my = now[0] + move2[d][0]
-        mx = now[1] + move2[d][1]
-        if R > my > -1 and C > mx > -1:
-            temp2 = board[flag][my][mx]
-            board[flag][my][mx] = temp1
-            temp1 = temp2
-        elif board[flag][my][mx] == -1:
+        pan[x][y], temp = temp, pan[x][y]
+        x,y = nx, ny
+    x, y = b, 1
+    index = 1
+    temp = 0
+    while True:
+        nx = x+dx[index]
+        ny = y+dy[index]
+        if nx==R or ny == C or nx==-1 or ny==-1:
+            index = (index+1)%4
+            continue
+        if x==b and y==0:
             break
-        else:
-            d += 1
+        pan[x][y], temp = temp, pan[x][y]
+        x,y = nx, ny
 
-R, C, T = map(int, sys.stdin.readline().split())
-board = [[],[]]
-dust = deque([])
-output = 0
-
-move1 = [[0,1],[-1,0],[0,-1],[1,0]]
-move2 = [[0,1],[1,0],[0,-1],[-1,0]]
-for i in range(R):
-    board[1].append([0] * C)
-    line = list(map(int, sys.stdin.readline().split()))
-    for c in range(C):
-        if line[c] > 0:
-            dust.append([r,c,1,0])
-        elif line[c] == -1:
-            muchine = r
-            board[1][r][c] = -1
-            board[1][r - 1][c] = -1
-    board[0].append(line)
-
-search()
+for i,line in enumerate(pan):
+        for j, value in enumerate(line):
+            if value>0:
+                cur.append((i,j,value))
+            if value==-1:
+                cleaner.append((i,j))
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+a = cleaner[0][0]
+b = cleaner[1][0]
+for t in range(T):
+    spread()
+    clean(a,b)
+    for i,line in enumerate(pan):
+        for j, value in enumerate(line):
+            if value>0:
+                cur.append((i,j,value))
+print(sum(cur[i][2] for i in range(len(cur))))
