e9947d019322c64de0af1a7c5533ca3968da137a
parkseokwon <parkswon1@naver.com>
2024-03-19 14:38:21 +0900

    모음 사전

---
+++ 프로그래머스/모음_사전.py
@@ -0,0 +1,18 @@
+def solution(word):
+    global answer, count
+    count = 0
+    recurcive("",word)
+    return answer
+
+def recurcive(words,word):
+    global answer, count
+    if words == word:
+        answer = count
+        return
+    elif len(words) != 5:
+        for a in alpha:
+            count += 1
+            recurcive(words+a,word)
+
+alpha = ["A","E","I","O","U"]
+print(solution("I"))
