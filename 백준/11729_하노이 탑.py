f0e240327b634405c08343c42cd9a8e84a61a585
parkseokwon <parkswon1@naver.com>
2024-03-21 14:58:12 +0900

    하노이의 탑

--- 백준/11729_하노이 탑.py
+++ 백준/11729_하노이 탑.py
@@ -1,14 +1,23 @@
 def solution(n):
     hano(n,1,3,2)
-    return answer
 
 def hano(num, now, to, ex):
+    global count
     if num == 1:
+        count += 1
         answer.append([now,to])
         return
 
     hano(num-1, now, ex, to)
+    count += 1
     answer.append([now,to])
     hano(num-1, ex, to, now)
 
 answer = []
+n = int(input())
+count = 0
+solution(n)
+
+print(count)
+for a in answer:
+    print(*a)
