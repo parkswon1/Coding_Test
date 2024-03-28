2ff8262aba7274838cbf8088645d7920ad3de513
parkseokwon <parkswon1@naver.com>
2024-03-28 09:42:35 +0900

    구명보트

---
+++ 프로그래머스/구명보트.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people = deque(people)
+    while(len(people) > 1):
+        back = people.pop()
+        if back + people[0] <= limit:
+            people.popleft()
+        answer += 1
+
+    if len(people) == 1 and people.pop() <= limit:
+        answer += 1
+
+    return answer
+
+people = [70, 80, 50]
+limit = 100
+print(solution(people, limit))
