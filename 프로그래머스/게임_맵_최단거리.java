b9a157884d66d7d69fa306da59a498509b557104
parkseokwon <parkswon1@naver.com>
2024-04-04 16:44:43 +0900

    게임_맵_최단거리

--- 프로그래머스/게임_맵_최단거리.java
+++ 프로그래머스/게임_맵_최단거리.java
@@ -5,7 +5,11 @@ import java.util.Deque;
 
 public class 게임_맵_최단거리 {
     public static void main(String[] args){
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
+        int[][] maps = {{1,0,1,1,1},
+                        {1,0,1,0,1},
+                        {1,0,1,1,1},
+                        {1,1,1,0,0},{
+                        0,0,0,0,1}};
         System.out.println(solution(maps));
     }
 
