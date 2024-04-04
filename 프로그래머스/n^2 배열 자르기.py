c631a6cfdceaf0a48e967f477150ef18cfc72fef
parkseokwon <parkswon1@naver.com>
2024-04-04 20:13:21 +0900

    n^2 배열 자르기

--- 프로그래머스/n^2 배열 자르기.py
+++ 프로그래머스/n^2 배열 자르기.py
@@ -1,24 +1,10 @@
-from collections import deque
-
 def solution(n, left, right):
     answer = []
-    board = [[1 for i in range(n)] for j in range(n)]
-    node = deque([[0,0,1]])
-    move = [[1,0],[0,1],[1,1]]
-    while(node):
-        y, x, z = node.popleft()
-        for mv in move:
-            my = y + mv[0];
-            mx = x + mv[1];
-            mz = z + 1;
-            if 0 <= my < n and 0 <= mx < n and board[my][mx] == 1:
-                board[my][mx] = mz
-                node.append([my, mx, mz])
-    for l in board:
-        answer += l
-    return answer[left:right+1]
+    for i in range(left, right + 1):
+        answer.append(max(i // n, i % n) + 1)
+    return answer
 
-n = 3
-left = 2
-right = 5
+n = 4
+left = 4
+right = 14
 print(solution(n, left, right))
