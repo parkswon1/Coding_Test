e498ca1ef4fbd4ee6cf71681707746150e17b33a
parkseokwon <parkswon1@naver.com>
2024-04-09 16:48:53 +0900

    기능개발

--- 프로그래머스/기능개발.py
+++ 프로그래머스/기능개발.py
@@ -5,20 +5,20 @@ def solution(progresses, speeds):
     progresses = deque(progresses)
     speeds = deque(speeds)
     day = 1
+    count = 0
     while(progresses):
-        count = 0
-        while(progresses):
-            if progresses[0] + speeds[0] * day >= 100:
-                progresses.popleft()
-                speeds.popleft()
-                count += 1
-            else:
-                break
-        if count != 0:
-            answer.append(count)
-        day += 1
+        if progresses[0] + speeds[0] * day >= 100:
+            progresses.popleft()
+            speeds.popleft()
+            count += 1
+        else:
+            if count != 0:
+                answer.append(count)
+                count = 0
+            day += 1
+    answer.append(count)
     return answer
 
-progress = [95, 90, 99, 99, 80, 99]
-speeds = [1, 1, 1, 1, 1, 1]
+progress = [93, 30, 55]
+speeds = [1, 30, 5]
 print(solution(progress, speeds))
