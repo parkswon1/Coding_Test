f5528fdef90c4c0f597dbf3948298ca85fe54f0f
parkseokwon <parkswon1@naver.com>
2024-05-16 10:41:32 +0900

    이중우선순위큐

---
+++ 프로그래머스/이중우선순위큐.py
@@ -0,0 +1,23 @@
+import heapq
+
+def solution(operations):
+    h = []
+
+    for i in operations:
+        a, b = i.split()
+        if a == 'I':
+            heapq.heappush(h, int(b))
+        else:
+            if len(h) > 0:
+                if b == '1':
+                    h.pop(h.index(heapq.nlargest(1, h)[0]))
+                else:
+                    heapq.heappop(h)
+
+    if len(h) == 0:
+        return [0, 0]
+    else:
+        return [heapq.nlargest(1, h)[0], h[0]]
+
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+print(solution(operations))
