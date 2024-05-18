40d0cb5e2bcb51ae05d9574e209563d5874f1378
parkseokwon <parkswon1@naver.com>
2024-05-18 17:38:46 +0900

    입국심사

---
+++ 프로그래머스/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    start = 1
+    end = min(times) * n
+    while start <= end:
+        mid = (start + end) // 2
+
+        count = 0
+        for t in times:
+            count += mid // t
+
+        if n > count:
+            start = mid  + 1
+        else:
+            end = mid - 1
+
+    return start
