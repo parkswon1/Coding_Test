6f9817f00af615d77f18505c15e3d112a63eca91
parkseokwon <parkswon1@naver.com>
2024-05-31 21:08:17 +0900

    1520_내리막 길

--- 백준/1520_내리막 길.py
+++ 백준/1520_내리막 길.py
@@ -1,34 +1,30 @@
 import sys
 sys.setrecursionlimit(100000)
 M,N = map(int,sys.stdin.readline().split())
-N,M = M,N # 편하게 보기 위해 M,N 스왑
-graph = []
+miro = []
+DP = []
+for i in range(M):
+    miro.append(list(map(int,sys.stdin.readline().split())))
 
-for i in range(N):
-    temt = list(map(int,sys.stdin.readline().split()))
-    graph.append(temt)
+DP = [[-1] * N for _ in range(M)]
 
-DP = [ [-1] *M for _ in range(N)]
+def dfs(y, x):
 
-def dfs(x,y):
-
-    if (x,y) == (N-1,M-1):
+    if (y, x) == (M-1, N-1):
         return 1
 
-    if DP[x][y]==-1:
-        DP[x][y]=0
+    if DP[y][x]==-1:
+        DP[y][x]=0
 
-        dx = [1,0,-1,0]
-        dy = [0,1,0,-1]
+        move = [(0,1),(0,-1),(1,0),(-1,0)]
 
-        for i in range(4):
+        for dy, dx in move:
+            my = y + dy
+            mx = x + dx
 
-            nx = x + dx[i]
-            ny = y + dy[i]
+            if 0 <= my < M and 0 <= mx < N and miro[my][mx] < miro[y][x]:
+                DP[y][x] += dfs(my, mx)
 
-            if 0<=nx<N and 0<=ny<M and graph[nx][ny]<graph[x][y]:
-                DP[x][y] += dfs(nx,ny)
-
-    return DP[x][y]
+    return DP[y][x]
 
 print(dfs(0,0))
