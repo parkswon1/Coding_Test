e072f066cbf13bdca91f516071d096f3f995962f
parkseokwon <parkswon1@naver.com>
2024-06-12 23:19:21 +0900

    17837_새로운 게임 2.py

---
+++ 백준/17837_새로운 게임 2.py
@@ -0,0 +1,81 @@
+file_path = "../input.txt"  # 상위 폴더에 있는 경우 상황에 맞게 경로를 조절해주세요.
+
+# 파일 읽어오기 (인코딩을 명시적으로 지정)
+with open(file_path, 'r', encoding='utf-8') as file:
+    input_data = file.read()
+
+# 문자열을 줄 단위로 나누어 리스트로 변환
+input_lines = input_data.strip().split('\n')
+
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+board = []
+pieceOnBoard = []
+pieces = []
+
+for n in range(N):
+    board.append(list(map(int, sys.stdin.readline().split())))
+    pieceOnBoard.append([[] for i in range(N)])
+
+for k in range(K):
+    y, x, v = map(int, sys.stdin.readline().split())
+    pieceOnBoard[y - 1][x - 1].append(k + 1)
+    pieces.append([y - 1, x - 1, v - 1])
+
+answer = -1
+move = [(0,1),(0,-1),(-1,0),(1,0)]
+def changV(v):
+    if v == 0:
+        return 1
+    elif v == 1:
+        return 0
+    elif v == 2:
+        return 3
+    else:
+        return 2
+
+
+# 0흰색, 1 빨간색, 2 파란색
+flag = 0
+while(1):
+    answer += 1
+    if flag == 1:
+        print(answer)
+        break
+    elif answer > 1000:
+        print(-1)
+        break
+
+    for k in range(K):
+        y, x, v = pieces[k]
+        number = k + 1
+
+        my = y + move[v][0]
+        mx = x + move[v][1]
+        index = pieceOnBoard[y][x].index(number)
+        if 0 > my or my >= N or 0 > mx or mx >= N or board[my][mx] == 2:
+            v = changV(v)
+            my = y + move[v][0]
+            mx = x + move[v][1]
+            pieces[k][2] = v
+
+        if 0 <= my < N and 0 <= mx < N and board[my][mx] != 2:
+            if board[my][mx] == 0:
+                pieceOnBoard[my][mx] += pieceOnBoard[y][x][index:]
+                for _ in range(len(pieceOnBoard[y][x]) - index):
+                    popedPiece = pieceOnBoard[y][x].pop()
+                    pieces[popedPiece - 1][0] = my
+                    pieces[popedPiece - 1][1] = mx
+
+            elif board[my][mx] == 1:
+                for _ in range(len(pieceOnBoard[y][x]) - index):
+                    popedPiece = pieceOnBoard[y][x].pop()
+                    pieceOnBoard[my][mx].append(popedPiece)
+                    pieces[popedPiece - 1][0] = my
+                    pieces[popedPiece - 1][1] = mx
+
+            if len(pieceOnBoard[my][mx]) >= 4:
+                flag = 1
+                break
