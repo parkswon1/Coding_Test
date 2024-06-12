ca0ec70fba4574d08d9cc2a29ce6f610c095066e
parkseokwon <parkswon1@naver.com>
2024-06-12 23:51:25 +0900

    17837_새로운 게임 2.py

--- 백준/17837_새로운 게임 2.py
+++ 백준/17837_새로운 게임 2.py
@@ -1,12 +1,3 @@
-file_path = "../input.txt"  # 상위 폴더에 있는 경우 상황에 맞게 경로를 조절해주세요.
-
-# 파일 읽어오기 (인코딩을 명시적으로 지정)
-with open(file_path, 'r', encoding='utf-8') as file:
-    input_data = file.read()
-
-# 문자열을 줄 단위로 나누어 리스트로 변환
-input_lines = input_data.strip().split('\n')
-
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
