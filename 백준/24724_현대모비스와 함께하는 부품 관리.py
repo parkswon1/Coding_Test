52913c43ee52b9a852e0ee5585f2703a815bee47
parkseokwon <parkswon1@naver.com>
2024-06-26 22:56:16 +0900

    24724_현대모비스와 함께하는 부품 관리.py

---
+++ 백준/24724_현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,11 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for t in range(T):
+    N = int(sys.stdin.readline())
+    A, B = map(int, sys.stdin.readline().split())
+    for _ in range(N):
+        a, b = map(int, sys.stdin.readline().split())
+    print("Material Management " + str(t+1))
+    print("Classification ---- End!")
