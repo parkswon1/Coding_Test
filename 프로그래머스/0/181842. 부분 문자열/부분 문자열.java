de54db666b62011ff58664804ce5331ec96dfb53
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2024-10-14 22:28:52 +0900

    [level 0] Title: 부분 문자열, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        if(str2.contains(str1)){
+            return 1;
+        }
+        return 0;
+    }
+}
