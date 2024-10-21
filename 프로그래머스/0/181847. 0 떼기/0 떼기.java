3403ecca66aec95744e0eb858132ae3a4d7f0f14
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2024-10-21 22:00:49 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String n_str) {
+        int i = 0;
+        
+        while (i < n_str.length() && n_str.charAt(i) == '0') {
+            i++;
+        }
+
+        return n_str.substring(i);
+    }
+}
