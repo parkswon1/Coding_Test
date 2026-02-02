5f675f19b9ab8d6536ca074578e9d8738f97e23b
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-02-02 22:48:39 +0900

    [level 2] Title: 롤케이크 자르기, Time: 795.54 ms, Memory: 50.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,32 @@
+#dict를 2개를 두고 값을 담는다.
+#dict의 key값이 있는경우 +1 0이 될경우 dict에서 아예 뺴버린다.
+#처음에 왼쪽놈한테 dict를 한번 싹 담는다.
+#이제 한번더 for문을 돌려서 오른쪽놈한테 dict를 1개씩 양보하면서 양쪽 dict의 길이를 비교하여 count 정답을 센다 같으면 +1
+
+from collections import defaultdict
+
+def solution(topping):
+    answer = 0
+    leftDict = {}
+    rightDict = {}
+    
+    for t in topping:
+        if t in leftDict:
+            leftDict[t] += 1
+        else:
+            leftDict[t] = 1
+    
+    for t in topping:
+        leftDict[t] -= 1
+        if leftDict[t] == 0:
+            leftDict.pop(t)
+        
+        if t in rightDict:
+            rightDict[t] += 1
+        else:
+            rightDict[t] = 1
+        
+        if len(leftDict) == len(rightDict):
+            answer += 1 
+    
+    return answer
