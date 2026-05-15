6d1c7791454cfe20af37ecbdc6d2b18abcc3d1cb
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-05-15 10:31:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 200.65 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,25 +1,30 @@
-def solution(s):
-    n = len(s)
-    if n == 0:
-        return 0
+from collections import deque
 
-    s2 = s + s  
-    openSet = {'(', '[', '{'}
-    pairMap = {')': '(', ']': '[', '}': '{'}
+rightSet = [['(',')'], ['[',']'], ['{','}']]
 
-    def isValid(segment):
-        stack = []
-        for ch in segment:
-            if ch in openSet:
-                stack.append(ch)
-            else:
-                if not stack or stack[-1] != pairMap.get(ch, ''):
-                    return False
+#검증하는 함수를 만들어서 검증하면 될듯
+def isTrue(S):
+    stack = []
+    for s in S:
+        stack.append(s)
+        if len(stack) > 1:
+            if [stack[-2],stack[-1]] in rightSet:
                 stack.pop()
-        return not stack
+                stack.pop()
+    
+    if len(stack) > 0:
+        return False
+    return True
+            
 
+def solution(s):
     answer = 0
-    for i in range(n):
-        if isValid(s2[i:i + n]):
+    #회전시키기 아마도 deque를 사용해서 list를 회전시키면 될듯
+    s = deque(s)
+    for i in range(len(s)):
+        if isTrue(s):
             answer += 1
+        s.append(s.popleft())
+        
+    #회전시키면서 검증하기
     return answer
