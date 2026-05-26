c6db0559f5e187226bfac3bff2a03cb6605aed2c
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-05-26 12:11:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 9.11 ms
+메모리: 10.1 MB, 시간: 12.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 21:58:30
+2026년 05월 26일 12:11:01
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
-    move = [(0,1),(1,0),(-1,0),(0,-1)]
+    answer = float('inf')
+    move = [(1,0),(-1,0),(0,1),(0,-1)]
+    nodes = deque([(0, 0, 1)])
+    visited = set()
+    visited.add((0,0))
     
-    N = len(maps)
-    M = len(maps[0])
-    
-    nodes = deque([(0,0,1)])
-    maps[0][0] = 0
     while(nodes):
-        y, x, count = nodes.popleft()
-        if y == N - 1 and x == M - 1:
-            return count
-        
-        for mv in move:
-            my = y + mv[0]
-            mx = x + mv[1]
+        y, x, cost = nodes.popleft()
+        for m in move:
+            my = m[0] + y
+            mx = m[1] + x
             
-            if 0 <= my < N and 0 <= mx < M:
-                if maps[my][mx] == 1:
-                    maps[my][mx] = 0
-                    nodes.append((my, mx, count + 1))
+            if 0 <= my < len(maps) and 0 <= mx < len(maps[0]):
+                if (my, mx) in visited or maps[my][mx] == 0:
+                    continue
+                if (my, mx) == (len(maps) - 1, len(maps[0]) - 1):
+                    return cost + 1
+                visited.add((my,mx))
+                nodes.append((my, mx, cost + 1))                
     
     return -1
