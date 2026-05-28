a71123df3d807938171dfc6bd93181430f3951b8
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-05-28 11:51:41 +0900

    [level 2] Title: 배달, Time: 1.31 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,29 +1,31 @@
-from collections import defaultdict
 import heapq
 
-def solution(N, roads, K):
+def solution(N, road, K):
     answer = 0
-    nodes = defaultdict(list)
-    for x, y, z in roads:
-        nodes[x].append([y, z])
-        nodes[y].append([x, z])
+    visitCost = [float('inf')] * (N+1)
+    costs = {}
+    for a,b,c in road:
+        if a not in costs:
+            costs[a] = [(b,c)]
+        else:
+            costs[a].append((b,c))
+        if b not in costs:
+            costs[b] = [(a,c)]
+        else:
+            costs[b].append((a,c))
     
-    dist = [float('inf')] * (N + 1)
-    dist[1] = 0
+    nodes = []
+    heapq.heappush(nodes, (0,1)) #거리 도시
+    visitCost[1] = 0
     
-    q = [(0,1)]
-    
-    while q:
-        d, curr = heapq.heappop(q)
-        
-        if dist[curr] < d:
-            continue
-        
-        for nxt, w in nodes[curr]:
-            cost = d + w
-            if cost < dist[nxt]:
-                dist[nxt] = cost
-                heapq.heappush(q, (cost, nxt))
-        
+    while(nodes):
+        currentCost, node = heapq.heappop(nodes)
+        for nextNode, cost in costs[node]:
+            if currentCost + cost < visitCost[nextNode] and currentCost + cost <= K:
+                visitCost[nextNode] = currentCost + cost
+                heapq.heappush(nodes, (visitCost[nextNode], nextNode))
+    for v in visitCost:
+        if v <= K:
+            answer += 1
 
-    return len([i for i in dist if i <= K])
+    return answer
