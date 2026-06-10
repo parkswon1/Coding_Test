68e466ec6eca7a2055ab81ffb4250d3e62aa5ca8
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-06-10 10:43:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 284.63 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,30 +1,24 @@
-#dict를 2개를 두고 값을 담는다.
-#dict의 key값이 있는경우 +1 0이 될경우 dict에서 아예 뺴버린다.
-#처음에 왼쪽놈한테 dict를 한번 싹 담는다.
-#이제 한번더 for문을 돌려서 오른쪽놈한테 dict를 1개씩 양보하면서 양쪽 dict의 길이를 비교하여 count 정답을 센다 같으면 +1
-
-from collections import defaultdict
-
 def solution(topping):
     answer = 0
-    leftDict = {}
-    rightSet = set()
+    frontSet = set()
+    backSet = set()
+    backDict = {}
     
     for t in topping:
-        if t in leftDict:
-            leftDict[t] += 1
+        backSet.add(t)
+        if t in backDict:
+            backDict[t] += 1
         else:
-            leftDict[t] = 1
+            backDict[t] = 1
     
     for t in topping:
-        leftDict[t] -= 1
-        if leftDict[t] == 0:
-            leftDict.pop(t)
+        frontSet.add(t)
+        if t in backDict:
+            backDict[t] -= 1
+            if backDict[t] <= 0:
+                backSet.remove(t)
         
-        rightSet.add(t)
-        
-        if len(leftDict) == len(rightSet):
+        if len(frontSet) == len(backSet):
             answer += 1
-        
     
     return answer
