7d7cde634c2a1845d59cca0a3e791b1aa3f1a89b
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-07-21 10:42:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.07 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,23 +2,24 @@ from collections import deque
 
 def solution(maps):
     answer = float('inf')
-    move = [(1,0),(-1,0),(0,1),(0,-1)]
-    nodes = deque([(0, 0, 1)])
+    move = [(1,0),(0,1),(-1,0),(0,-1)]
+    nodes = deque([(0,0,1)]) #y,x,count
     visited = set()
     visited.add((0,0))
-    
-    while(nodes):
-        y, x, cost = nodes.popleft()
-        for m in move:
-            my = m[0] + y
-            mx = m[1] + x
-            
+    while nodes:
+        y,x,count = nodes.popleft()
+        count += 1
+        for dy, dx in move:
+            my = dy + y
+            mx = dx + x
             if 0 <= my < len(maps) and 0 <= mx < len(maps[0]):
                 if (my, mx) in visited or maps[my][mx] == 0:
                     continue
                 if (my, mx) == (len(maps) - 1, len(maps[0]) - 1):
-                    return cost + 1
+                    answer = min(answer, count)
+                nodes.append((my,mx,count))
                 visited.add((my,mx))
-                nodes.append((my, mx, cost + 1))                
-    
-    return -1
+
+    if answer == float('inf'):
+        return -1
+    return answer
