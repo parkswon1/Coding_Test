28e5a009d20eb46df6ed510604f21d9f5152a23d
Seok Won Park <74632742+parkswon1@users.noreply.github.com>
2026-07-22 18:04:31 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.43 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,17 @@
+import heapq
+def solution(jobs):
+    jobs.sort()
+    n = len(jobs)
+    heap = []
+    now = ans = 0
+    while jobs or heap:
+        while jobs and now >= jobs[0][0]:
+            heapq.heappush(heap,jobs.pop(0)[::-1])
+        if jobs and not heap:
+            i = jobs.pop(0)
+            now = i[0]
+            heapq.heappush(heap, i[::-1])
+        j = heapq.heappop(heap)
+        now += j[0]
+        ans += now-j[-1]
+    return ans//n
